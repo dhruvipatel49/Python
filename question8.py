640199d23d8e91698330214193fdb4f8169807b2
dhruvipatel49 <dhruvialpeshpatel@gmail.com>
2025-03-24 21:24:52 +0530

    Delete question8.py

--- question8.py
+++
@@ -1,9 +0,0 @@
-def convert(string):
-    words=string.split()
-    unique_words=set(words)
-    sorted_words=sorted(unique_words)
-    result=' '.join(sorted_words)
-    return result
-str=input("Enter the string:")
-check=convert(str)
-print(check)
