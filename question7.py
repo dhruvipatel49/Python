6442d76e2fa6e9a0129680b58aa38cda501d35ab
dhruvipatel49 <dhruvialpeshpatel@gmail.com>
2025-03-24 21:24:40 +0530

    Delete question7.py

--- question7.py
+++
@@ -1,9 +0,0 @@
-def ispalindrome(string1):
-    string2=string1[::-1]
-    if(string1==string2):
-        print("THe given string is palindrome.")
-    else:
-        print("The given string is not palindrome.")
-str=input("Enter the string:")
-check_palindrome=ispalindrome(str)
-print(check_palindrome)
